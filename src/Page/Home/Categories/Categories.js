04c14ea249a2c1e589885b6ed2853c9bdc9aa8be
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-11-24 23:38:05 +0600

    category part added

--- src/Page/Home/Categories/Categories.js
+++ src/Page/Home/Categories/Categories.js
@@ -5,7 +5,7 @@ const Categories = () => {
           <div className=' flex '>
                <h1 className='text-2xl font-semibold ml-5 my-5'> Browse items by category </h1>
                <div>
-
+                    
                </div>
           </div>
      );
